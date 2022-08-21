6772b90379f303dae4011fb9d8e608683a8d35a0
Marco Aurélio Pessota <pessota.marco@gmail.com>
2022-08-21 15:56:18 -0300

    task: configuração de relacionamento de 1<>n entre student e phones

--- src/Entity/Phone.php
+++ src/Entity/Phone.php
@@ -6,17 +6,27 @@ use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\ManyToOne;
 
 #[Entity]
 Class Phone
 {
     #[Id, GeneratedValue, Column]
     public int $id;
+    
+    #[ManyToOne(targetEntity: Student::class, inversedBy: "phones")]
+    public Student $student;
+
     #[Column]
     public string $number;
 
     public function __construct(string $number)
     {
         $this->number = $number;
+    }
+
+    public function setStudent(Student $student)
+    {
+        $this->student = $student;
     }
 }
--- src/Entity/Student.php
+++ src/Entity/Student.php
@@ -2,21 +2,41 @@
 
 namespace Alura\Doctrine\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping\Column;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\Mapping\GeneratedValue;
 use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\OneToMany;
 
 #[Entity]
 Class Student
 {
     #[Id, GeneratedValue, Column]
     public int $id;
+
+    #[OneToMany(targetEntity: Phone::class, mappedBy:"student")]
+    private Collection $phones;
+
     #[Column]
     public string $name;
 
     public function __construct(string $name)
     {
         $this->name = $name;
+        $this->phones = new ArrayCollection();
+    }
+    
+    function addPhone(Phone $phone)
+    {
+        $this->phones->add($phone);
+        $phone->setStudent($this);
+    }
+
+    /** @return iterable<Phone> */
+    function phones()
+    {
+        return $this->phones;
     }
 }
