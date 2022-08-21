9986eb45c5e6489122fdfd8c210c23017ffee143
Marco Aurélio Pessota <pessota.marco@gmail.com>
2022-08-21 16:11:19 -0300

    task: ajustando para trabalhar com delete em cascata

--- bin/delete-student.php
+++ bin/delete-student.php
@@ -6,8 +6,8 @@ use Alura\Doctrine\Helper\EntityManagerCreator;
 use Alura\Doctrine\Entity\Student;
 
 $entityManager = EntityManagerCreator::createEntityManager();
-// $student = $entityManager->find(Student::class,$argv[1]); // Traz todos os dados do registro
-$student = $entityManager->getPartialReference(Student::class, $argv[1]); // Esse comando traz apenas o id
+$student = $entityManager->find(Student::class,$argv[1]); // Traz todos os dados do registro, precisa para excluir dados em cascata
+// $student = $entityManager->getPartialReference(Student::class, $argv[1]); // Esse comando traz apenas o id
 
 $entityManager->remove($student);
 $entityManager->flush();
