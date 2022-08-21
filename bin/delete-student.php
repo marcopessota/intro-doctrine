06d29a9e468d96b09fb6563ed1e9d98f4a643577
Marco Aurélio Pessota <pessota.marco@gmail.com>
2022-08-21 15:10:51 -0300

    task: Script para deletar student por pela doctrine

---
+++ bin/delete-student.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Alura\Doctrine\Helper\EntityManagerCreator;
+use Alura\Doctrine\Entity\Student;
+
+$entityManager = EntityManagerCreator::createEntityManager();
+// $student = $entityManager->find(Student::class,$argv[1]); // Traz todos os dados do registro
+$student = $entityManager->getPartialReference(Student::class, $argv[1]); // Esse comando traz apenas o id
+
+$entityManager->remove($student);
+$entityManager->flush();
