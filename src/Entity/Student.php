802391d8203f3e0fc3aa022d6c929473f040f65d
Marco Aurélio Pessota <pessota.marco@gmail.com>
2022-08-20 22:38:08 -0300

    task: Criação do entity manager e da entidade Student

---
+++ src/Entity/Student.php
@@ -0,0 +1,24 @@
+<?php 
+
+namespace Alura\Doctrine\Entity;
+
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+
+#[Entity]
+Class Student
+{
+    #[Id]
+    #[GeneratedValue]
+    #[Column]
+    public int $id;
+    #[Column]
+    public string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+}
