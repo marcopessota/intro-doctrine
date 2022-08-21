709ef4c23051506aa2703fd65a2474e0c8e611ca
Marco Aurélio Pessota <pessota.marco@gmail.com>
2022-08-21 15:56:52 -0300

    feat: implementação de possibilidade de salvar student com telefone

--- bin/insert-student.php
+++ bin/insert-student.php
@@ -2,10 +2,20 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Alura\Doctrine\Entity\Phone;
 use Alura\Doctrine\Helper\EntityManagerCreator;
 use Alura\Doctrine\Entity\Student;
 
 $entityManager = EntityManagerCreator::createEntityManager();
 
-$entityManager->persist(new Student($argv[1]));
+$student = new Student($argv[1]);
+$entityManager->persist($student);
+
+if(!empty($argv[2])){
+    $phone = new Phone($argv[2]);
+    $entityManager->persist($phone);
+
+    $student->addPhone($phone);
+} 
+
 $entityManager->flush();
